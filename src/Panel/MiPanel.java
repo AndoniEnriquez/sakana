cd12f2268b8e6839e835e178021bbe770639907e
Anthonyenri <Anthonyenri@localhost>
2016-05-18 08:29:07 +0200

    V 1.1

---
+++ src/Panel/MiPanel.java
@@ -0,0 +1,32 @@
+package Panel;
+
+import java.awt.Graphics;
+import java.awt.Image;
+
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class MiPanel extends JPanel{
+
+	Image imagen;
+	public MiPanel(Image imagen){
+		
+		if (imagen != null) {
+			this.imagen = imagen;
+		}
+	}
+	
+	@Override
+	
+	public void paint(Graphics g) {
+
+		if (imagen != null) {
+			g.drawImage(imagen, 0, 0, getWidth(), getHeight(),this);
+			setOpaque(false);
+		} else {
+			setOpaque(true);
+		}	
+		super.paint(g);
+	}
+
+}
---
+++ src/Panel/PanelVentana.java
@@ -0,0 +1,20 @@
+package Panel;
+
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+@SuppressWarnings("serial")
+public class PanelVentana extends PanelExample implements ChangeListener{
+
+	public PanelVentana(int tamX, int tamY) {
+		super(tamX, tamY);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void stateChanged(ChangeEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
