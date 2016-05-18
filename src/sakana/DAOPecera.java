efc609363144eb8b549774649d475addcfc1b541
Andoni Alzelai <andoni.alcelay@alumni.mondragon.edu>
2016-05-18 18:01:45 +0200

    Pool conexiones, DAOPecera, pecera y Medicion

---
+++ src/sakana/DAOPecera.java
@@ -0,0 +1,162 @@
+package sakana;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Calendar;
+
+public class DAOPecera {
+
+	static public ArrayList<Pecera> getPeceras() throws Exception{
+		Statement stmt;
+		ResultSet result;
+		String strSQL;
+		ArrayList<Pecera> lista = null;
+		Pecera pecera;
+		try
+		{
+
+			lista = new ArrayList<>();
+			stmt=PoolConexiones.getConexion().createStatement();
+			strSQL="SELECT pecera_id,IP,Capacidad,horacomida"+
+					" FROM PECERA";
+			result = stmt.executeQuery(strSQL);
+			while (result.next()){
+				Calendar cal = Calendar.getInstance();
+				cal.setTime(result.getDate("horacomida"));
+					
+				pecera = new Pecera(result.getInt("pecera_id"),result.getString("IP"),
+						result.getInt("Capacidad"),cal);
+				lista.add(pecera);
+			}
+			result.close();
+		} catch (Exception e ){
+			e.printStackTrace();
+		}
+		return lista;
+	}
+
+	static public Pecera buscarPorID(int idPecera) throws Exception
+	{
+		Statement stmt;
+		ResultSet result;
+		String strSQL;
+		Pecera p;
+
+		try
+		{
+			stmt=PoolConexiones.getConexion().createStatement();
+			strSQL="SELECT pecera_id, IP, Capacidad, horacomida"+
+					" FROM PECERA"+
+					" WHERE pecera_id="+idPecera;
+			result = stmt.executeQuery(strSQL);
+			if(!result.next()) return null;
+			
+			Calendar cal = Calendar.getInstance();
+			cal.setTime(result.getDate("horacomida"));
+			p=new Pecera(result.getInt("pecera_id"),result.getString("IP"),
+					result.getInt("Capacidad"), cal);
+			result.close();
+			return p;
+		}
+
+		catch(SQLException e)
+		{
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	static public Pecera buscarPorIP(String IPPecera) throws Exception
+	{
+		Statement stmt;
+		ResultSet result;
+		String strSQL;
+		Pecera p;
+
+		try
+		{
+			stmt=PoolConexiones.getConexion().createStatement();
+			strSQL="SELECT pecera_id, IP, Capacidad, horacomida"+
+					" FROM PECERA"+
+					" WHERE IP="+IPPecera;
+			result = stmt.executeQuery(strSQL);
+			if(!result.next()) return null;
+			
+			Calendar cal = Calendar.getInstance();
+			cal.setTime(result.getDate("horacomida"));
+			p=new Pecera(result.getInt("pecera_id"),result.getString("IP"),
+					result.getInt("Capacidad"),cal);
+			result.close();
+			return p;
+		}
+
+		catch(SQLException e)
+		{
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+
+	static public boolean addPecera(Pecera p) throws Exception{
+
+		Statement stmt;
+		String strSQL;
+		int result;
+
+		try
+		{
+			if(buscarPorID(p.getID())==null && buscarPorIP(p.getIP()) == null)
+			{
+
+				stmt=PoolConexiones.getConexion().createStatement();
+				strSQL="INSERT INTO PECERA "+
+						" VALUES ("+p.getID()+",'"+p.getIP()+"','"+p.getCapacidad()+"','"+p.getHoracomida().getTime()+")";
+				result = stmt.executeUpdate(strSQL);		      
+				return true;
+			}
+			else return false;
+		}
+
+		catch(SQLException e)
+		{
+			e.printStackTrace();;
+			return false;
+		}
+	}
+
+	public static void eliminarPecera(String IP) throws SQLException {
+		Statement stmt;
+		int result;
+		String strSQL;
+		stmt=PoolConexiones.getConexion().createStatement();
+		strSQL="DELETE FROM  PECERA "+
+				" WHERE IP = '"+IP+"'";
+		result = stmt.executeUpdate(strSQL);
+	}
+
+	static public boolean updatePecera(Pecera p) throws Exception
+	{    
+		Statement stmt;
+		boolean ok=false;
+		String strSQL;
+
+		try
+		{
+			stmt=PoolConexiones.getConexion().createStatement();
+			strSQL="UPDATE PECERA "+
+					" SET IP = '"+p.getIP()+
+					"', Capacidad   = '"+p.getCapacidad()+
+					"', horacomida   = '"+p.getHoracomida().getTime()+
+					"' WHERE pecera_id="+p.getID();
+			return (stmt.executeUpdate(strSQL)>0);
+		}
+		catch(SQLException e)
+		{
+			e.printStackTrace();
+			return false;
+		}
+	}
+}
---
+++ src/sakana/Medicion.java
@@ -0,0 +1,63 @@
+package sakana;
+
+import java.util.Calendar;
+
+public class Medicion {
+	
+	int ID;
+	String nombre;
+	Calendar datetime;
+	int tipoMedicion_id;
+	int pecera_id;
+	
+	public Medicion(int ID, String nombre, Calendar datetime, int tipoMedicion_id, int pecera_id){
+		this.ID=ID;
+		this.nombre=nombre;
+		this.datetime=datetime;
+		this.tipoMedicion_id=tipoMedicion_id;
+		this.pecera_id=tipoMedicion_id;
+	}
+
+	public int getID() {
+		return ID;
+	}
+
+	public void setID(int iD) {
+		ID = iD;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Calendar getDatetime() {
+		return datetime;
+	}
+
+	public void setDatetime(Calendar datetime) {
+		this.datetime = datetime;
+	}
+
+	public int getTipoMedicion_id() {
+		return tipoMedicion_id;
+	}
+
+	public void setTipoMedicion_id(int tipoMedicion_id) {
+		this.tipoMedicion_id = tipoMedicion_id;
+	}
+
+	public int getPecera_id() {
+		return pecera_id;
+	}
+
+	public void setPecera_id(int pecera_id) {
+		this.pecera_id = pecera_id;
+	}
+	
+	
+
+}
