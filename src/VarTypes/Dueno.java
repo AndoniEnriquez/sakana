8c7e7db7bd0554aa38853d2b175dcd21c63feaea
HP <HP@Joseba>
2016-05-20 12:03:23 +0200

    a

--- src/VarTypes/Dueno.java
+++ src/VarTypes/Dueno.java
@@ -18,6 +18,14 @@ public class Dueno {
 	}
 
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
 	public String getNombreDueno() {
 		return nombreDueno;
 	}
