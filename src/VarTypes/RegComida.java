46047aa616f9efd960c2830e0bfd75666faf4a92
Andoni Alzelai <andoni.alcelay@alumni.mondragon.edu>
2016-05-20 14:45:40 +0200

    RegComida.java

--- src/VarTypes/RegComida.java
+++ src/VarTypes/RegComida.java
@@ -1,5 +1,48 @@
 package VarTypes;
 
-public class RegComida {
+import java.sql.Date;
 
+public class RegComida {
+	
+	int comida_id, pecera_id;
+	Date datetime;
+	
+	
+	public RegComida(int comida_id, int pecera_id, Date datetime){
+		this.comida_id = comida_id;
+		this.pecera_id = pecera_id;
+		this.datetime = datetime;
+	}
+
+
+	public int getComida_id() {
+		return comida_id;
+	}
+
+
+	public void setComida_id(int comida_id) {
+		this.comida_id = comida_id;
+	}
+
+
+	public int getPecera_id() {
+		return pecera_id;
+	}
+
+
+	public void setPecera_id(int pecera_id) {
+		this.pecera_id = pecera_id;
+	}
+
+
+	public Date getDatetime() {
+		return datetime;
+	}
+
+
+	public void setDatetime(Date datetime) {
+		this.datetime = datetime;
+	}
+
+	
 }
