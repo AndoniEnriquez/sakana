cfaa17aa6af8189023d9d941f0cadbac7510f9bb
Ander Gonzalez <ander.gonzalezt@alumni.mondragon.edu>
2016-05-24 10:29:11 +0100

    Interfaces de la comunicacion de la pecera falta el proceso de sincronizacion jajajaj ahora mismito lo hago XD que os peten

--- src/conexionMicro/ClienteMicro.java
+++ src/conexionMicro/ClienteMicro.java
@@ -5,46 +5,71 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
+import java.nio.charset.Charset;
+import java.util.Scanner;
+
+import javax.swing.plaf.synth.SynthSpinnerUI;
+
+import com.mysql.fabric.xmlrpc.Client;
 
 public class ClienteMicro {
 	String servidor;
 	int puerto;
-	
+
 	public ClienteMicro(String servidor, int puerto){
 		this.servidor = servidor;
 		this.puerto = puerto;
 	}
-	
+
 	public String enviarComando(String comando){
 		String respuesta = "";
-		char c;
+		Socket client = null;
 		if(!comando.endsWith(";")) comando = comando + ";";
-		 try {
-			Socket client = new Socket(servidor, puerto);
-			 OutputStream outToServer = client.getOutputStream();
-			 DataOutputStream out = new DataOutputStream(outToServer);
-			 
-			 char[] ca = comando.toCharArray();
-			 for(char a : ca){
-				 out.writeInt(a);
-			 }
-			 InputStream inFromServer = client.getInputStream();
-			 DataInputStream in = new DataInputStream(inFromServer);
-			 while((c = (char) in.read()) != ';'){
+		try {
+			client =  new Socket(servidor, puerto);
+			OutputStream outToServer = client.getOutputStream();
+			DataOutputStream out = new DataOutputStream(outToServer);
+			InputStream inFromServer = client.getInputStream();
+			DataInputStream in = new DataInputStream(inFromServer);
+			
+			byte[] b = comando.getBytes();
+			for(byte by : b){
+				out.write(by);		
+			}
+						
+			char c;
+			do {
+				 c = (char) in.read();
 				 respuesta = "" + respuesta + c;
-			 }
-			 respuesta = respuesta + ";";
+			 }while(c != ';');
 			 client.close();
 		} catch (Exception e) {
 			respuesta = null;
+			try {
+				client.close();
+			} catch (Exception e1 ) {
+			}
 		}	
 		return respuesta;
-		
+
 	}
-	
-	
-	
-	
-	
+
+
+	public static void main(String[] args) {
+		while(true){
+			ClienteMicro c = new ClienteMicro("192.168.1.15", 4444);
+			Scanner s = new Scanner(System.in);
+			while(true){
+				System.out.println(c.enviarComando(s.nextLine()));
+			}
+		}
+	}
+
+
+
+
+
 }
--- src/conexionMicro/InterfazMicro.java
+++ src/conexionMicro/InterfazMicro.java
@@ -1,6 +1,7 @@
 package conexionMicro;
 
 import java.util.Calendar;
+import java.util.Scanner;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -13,6 +14,7 @@ public class InterfazMicro {
 	private final int PORT = 4444;
 	
 	private String respuesta;
+	private ClienteMicro cliente;
 	
 	public static final int OK = 1;
 	public static final int NO_CONEXION = 2;
@@ -20,9 +22,13 @@ public class InterfazMicro {
 	public static final int CAMBIO_NO_REALIZADO = 4;
 	public static final int ERROR_DESCONOCIDO = 5;
 	
+	public InterfazMicro(Pecera p) {
+		cliente = new ClienteMicro(p.getIP(), PORT);
+	}
+	
 	@SuppressWarnings("deprecation")
-	int setDateTime(final Calendar c, Pecera p) {
-		final ClienteMicro cliente = new ClienteMicro(p.getIP(), PORT);
+	public int setDateTime(final Calendar c) {
+		
 		respuesta = null;
 		
 		Thread t = new Thread( new Runnable() {
@@ -42,9 +48,553 @@ public class InterfazMicro {
 		}
 		if (respuesta == null) return NO_CONEXION;
 		if(respuesta.startsWith("time")) {
-			System.out.println(respuesta);
 			return OK;
 		}
 		return ERROR_DESCONOCIDO;
 	}
+	
+	public Calendar getDateTime(){
+		respuesta = null;
+		
+		Thread t = new Thread( new Runnable() {
+			@Override
+			public void run() {
+				respuesta = cliente.enviarComando("get time;");				
+			}
+		});
+		
+		t.start();
+		long start = Calendar.getInstance().getTimeInMillis();
+		while(t.isAlive() && Calendar.getInstance().getTimeInMillis() - start < 2000 ){}
+		
+		if(t.isAlive()){
+			t.stop();
+			return null;
+		}
+		if (respuesta == null) return null;;
+		if(respuesta.startsWith("time")) {
+			respuesta = respuesta.replace(";", "");
+			String[] sa = respuesta.split(" ");
+			Long f = Long.parseLong(sa[1]);
+			Calendar c = Calendar.getInstance();
+			c.setTimeInMillis((long) (f * 1000));
+			return c;
+		}
+		return null;
+	}
+	
+	@SuppressWarnings("deprecation")
+	public int setFeedTime(final Calendar c) {
+		
+		respuesta = null;
+		
+		Thread t = new Thread( new Runnable() {
+			@Override
+			public void run() {
+				respuesta = cliente.enviarComando("set feedtime " + c.getTimeInMillis()/1000 +";");				
+			}
+		});
+		
+		t.start();
+		long start = Calendar.getInstance().getTimeInMillis();
+		while(t.isAlive() && Calendar.getInstance().getTimeInMillis() - start < 2000 ){}
+		
+		if(t.isAlive()){
+			t.stop();
+			return NO_RESPUESTA;
+		}
+		if (respuesta == null) return NO_CONEXION;
+		if(respuesta.startsWith("feedtime")) {
+			return OK;
+		}
+		return ERROR_DESCONOCIDO;
+	}
+	
+	public Calendar getFeedTime(){
+		respuesta = null;
+		
+		Thread t = new Thread( new Runnable() {
+			@Override
+			public void run() {
+				respuesta = cliente.enviarComando("get feedtime;");				
+			}
+		});
+		
+		t.start();
+		long start = Calendar.getInstance().getTimeInMillis();
+		while(t.isAlive() && Calendar.getInstance().getTimeInMillis() - start < 2000 ){}
+		
+		if(t.isAlive()){
+			t.stop();
+			return null;
+		}
+		if (respuesta == null) return null;;
+		if(respuesta.startsWith("feedtime")) {
+			respuesta = respuesta.replace(";", "");
+			String[] sa = respuesta.split(" ");
+			Long f = Long.parseLong(sa[1]);
+			Calendar c = Calendar.getInstance();
+			c.setTimeInMillis((long) (f * 1000));
+			return c;
+		}
+		return null;
+	}
+	
+	public int setMeals(final int meals){
+
+		respuesta = null;
+		
+		Thread t = new Thread( new Runnable() {
+			@Override
+			public void run() {
+				respuesta = cliente.enviarComando("set meals " + meals +";");				
+			}
+		});
+		
+		t.start();
+		long start = Calendar.getInstance().getTimeInMillis();
+		while(t.isAlive() && Calendar.getInstance().getTimeInMillis() - start < 2000 ){}
+		
+		if(t.isAlive()){
+			t.stop();
+			return NO_RESPUESTA;
+		}
+		if (respuesta == null) return NO_CONEXION;
+		if(respuesta.startsWith("meals")) {
+			return OK;
+		}
+		return ERROR_DESCONOCIDO;
+	}
+	
+	public int getMeals(){
+		respuesta = null;
+		
+		Thread t = new Thread( new Runnable() {
+			@Override
+			public void run() {
+				respuesta = cliente.enviarComando("get meals;");				
+			}
+		});
+		
+		t.start();
+		long start = Calendar.getInstance().getTimeInMillis();
+		while(t.isAlive() && Calendar.getInstance().getTimeInMillis() - start < 2000 ){}
+		
+		if(t.isAlive()){
+			t.stop();
+			return -1;
+		}
+		if (respuesta == null) return -1;;
+		if(respuesta.startsWith("meals")) {
+			respuesta = respuesta.replace(";", "");
+			String[] sa = respuesta.split(" ");
+			Integer i = Integer.parseInt(sa[1]);
+			return (int)i;
+		}
+		return -1;
+	}
+	
+	public int setFishNo(final int fishNo){
+
+		respuesta = null;
+		
+		Thread t = new Thread( new Runnable() {
+			@Override
+			public void run() {
+				respuesta = cliente.enviarComando("set meals " + fishNo +";");				
+			}
+		});
+		
+		t.start();
+		long start = Calendar.getInstance().getTimeInMillis();
+		while(t.isAlive() && Calendar.getInstance().getTimeInMillis() - start < 2000 ){}
+		
+		if(t.isAlive()){
+			t.stop();
+			return NO_RESPUESTA;
+		}
+		if (respuesta == null) return NO_CONEXION;
+		if(respuesta.startsWith("fishno")) {
+			return OK;
+		}
+		return ERROR_DESCONOCIDO;
+	}
+	
+	public int getFishNo(){
+		respuesta = null;
+		
+		Thread t = new Thread( new Runnable() {
+			@Override
+			public void run() {
+				respuesta = cliente.enviarComando("get fishno;");				
+			}
+		});
+		
+		t.start();
+		long start = Calendar.getInstance().getTimeInMillis();
+		while(t.isAlive() && Calendar.getInstance().getTimeInMillis() - start < 2000 ){}
+		
+		if(t.isAlive()){
+			t.stop();
+			return -1;
+		}
+		if (respuesta == null) return -1;;
+		if(respuesta.startsWith("fishno")) {
+			respuesta = respuesta.replace(";", "");
+			String[] sa = respuesta.split(" ");
+			Integer i = Integer.parseInt(sa[1]);
+			return (int)i;
+		}
+		return -1;
+	}
+	
+	public float getPh(){
+		respuesta = null;
+		
+		Thread t = new Thread( new Runnable() {
+			@Override
+			public void run() {
+				respuesta = cliente.enviarComando("get ph;");				
+			}
+		});
+		
+		t.start();
+		long start = Calendar.getInstance().getTimeInMillis();
+		while(t.isAlive() && Calendar.getInstance().getTimeInMillis() - start < 2000 ){}
+		
+		if(t.isAlive()){
+			t.stop();
+			return -1;
+		}
+		if (respuesta == null) return -1;;
+		if(respuesta.startsWith("ph")) {
+			respuesta = respuesta.replace(";", "");
+			String[] sa = respuesta.split(" ");
+			Float f = Float.parseFloat(sa[1]);
+			return f;
+		}
+		return -1;
+	}
+	
+	public float getPhOffset(){
+		respuesta = null;
+		
+		Thread t = new Thread( new Runnable() {
+			@Override
+			public void run() {
+				respuesta = cliente.enviarComando("get phoffset;");				
+			}
+		});
+		
+		t.start();
+		long start = Calendar.getInstance().getTimeInMillis();
+		while(t.isAlive() && Calendar.getInstance().getTimeInMillis() - start < 2000 ){}
+		
+		if(t.isAlive()){
+			t.stop();
+			return -1;
+		}
+		if (respuesta == null) return -1;;
+		if(respuesta.startsWith("phoffset")) {
+			respuesta = respuesta.replace(";", "");
+			String[] sa = respuesta.split(" ");
+			Float f = Float.parseFloat(sa[1]);
+			return f;
+		}
+		return -1;
+	}
+	
+	public int setPhOffset(final float phOffset){
+
+		respuesta = null;
+		
+		Thread t = new Thread( new Runnable() {
+			@Override
+			public void run() {
+				respuesta = cliente.enviarComando("set phoffset " + phOffset +";");				
+			}
+		});
+		
+		t.start();
+		long start = Calendar.getInstance().getTimeInMillis();
+		while(t.isAlive() && Calendar.getInstance().getTimeInMillis() - start < 2000 ){}
+		
+		if(t.isAlive()){
+			t.stop();
+			return NO_RESPUESTA;
+		}
+		if (respuesta == null) return NO_CONEXION;
+		if(respuesta.startsWith("phoffset")) {
+			return OK;
+		}
+		return ERROR_DESCONOCIDO;
+	}
+	
+	public float getPhMax(){
+		respuesta = null;
+		
+		Thread t = new Thread( new Runnable() {
+			@Override
+			public void run() {
+				respuesta = cliente.enviarComando("get phmax;");				
+			}
+		});
+		
+		t.start();
+		long start = Calendar.getInstance().getTimeInMillis();
+		while(t.isAlive() && Calendar.getInstance().getTimeInMillis() - start < 2000 ){}
+		
+		if(t.isAlive()){
+			t.stop();
+			return -1;
+		}
+		if (respuesta == null) return -1;;
+		if(respuesta.startsWith("phmax")) {
+			respuesta = respuesta.replace(";", "");
+			String[] sa = respuesta.split(" ");
+			Float f = Float.parseFloat(sa[1]);
+			return f;
+		}
+		return -1;
+	}
+	
+	public int setPhMax(final float phMax){
+
+		respuesta = null;
+		
+		Thread t = new Thread( new Runnable() {
+			@Override
+			public void run() {
+				respuesta = cliente.enviarComando("set phmax " + phMax +";");				
+			}
+		});
+		
+		t.start();
+		long start = Calendar.getInstance().getTimeInMillis();
+		while(t.isAlive() && Calendar.getInstance().getTimeInMillis() - start < 2000 ){}
+		
+		if(t.isAlive()){
+			t.stop();
+			return NO_RESPUESTA;
+		}
+		if (respuesta == null) return NO_CONEXION;
+		if(respuesta.startsWith("phmax")) {
+			return OK;
+		}
+		return ERROR_DESCONOCIDO;
+	}
+	
+	public float getPhMin(){
+		respuesta = null;
+		
+		Thread t = new Thread( new Runnable() {
+			@Override
+			public void run() {
+				respuesta = cliente.enviarComando("get phmin;");				
+			}
+		});
+		
+		t.start();
+		long start = Calendar.getInstance().getTimeInMillis();
+		while(t.isAlive() && Calendar.getInstance().getTimeInMillis() - start < 2000 ){}
+		
+		if(t.isAlive()){
+			t.stop();
+			return -1;
+		}
+		if (respuesta == null) return -1;;
+		if(respuesta.startsWith("phmin")) {
+			respuesta = respuesta.replace(";", "");
+			String[] sa = respuesta.split(" ");
+			Float f = Float.parseFloat(sa[1]);
+			return f;
+		}
+		return -1;
+	}
+	
+	public int setPhMin(final float phMin){
+
+		respuesta = null;
+		
+		Thread t = new Thread( new Runnable() {
+			@Override
+			public void run() {
+				respuesta = cliente.enviarComando("set phmin " + phMin +";");				
+			}
+		});
+		
+		t.start();
+		long start = Calendar.getInstance().getTimeInMillis();
+		while(t.isAlive() && Calendar.getInstance().getTimeInMillis() - start < 2000 ){}
+		
+		if(t.isAlive()){
+			t.stop();
+			return NO_RESPUESTA;
+		}
+		if (respuesta == null) return NO_CONEXION;
+		if(respuesta.startsWith("phmin")) {
+			return OK;
+		}
+		return ERROR_DESCONOCIDO;
+	}
+	
+	public float getTemp(){
+		respuesta = null;
+		
+		Thread t = new Thread( new Runnable() {
+			@Override
+			public void run() {
+				respuesta = cliente.enviarComando("get t;");				
+			}
+		});
+		
+		t.start();
+		long start = Calendar.getInstance().getTimeInMillis();
+		while(t.isAlive() && Calendar.getInstance().getTimeInMillis() - start < 2000 ){}
+		
+		if(t.isAlive()){
+			t.stop();
+			return -1;
+		}
+		if (respuesta == null) return -1;;
+		if(respuesta.startsWith("t")) {
+			respuesta = respuesta.replace(";", "");
+			String[] sa = respuesta.split(" ");
+			Float f = Float.parseFloat(sa[1]);
+			return f;
+		}
+		return -1;
+	}
+	
+	public float getTempMin(){
+		respuesta = null;
+		
+		Thread t = new Thread( new Runnable() {
+			@Override
+			public void run() {
+				respuesta = cliente.enviarComando("get tmin;");				
+			}
+		});
+		
+		t.start();
+		long start = Calendar.getInstance().getTimeInMillis();
+		while(t.isAlive() && Calendar.getInstance().getTimeInMillis() - start < 2000 ){}
+		
+		if(t.isAlive()){
+			t.stop();
+			return -1;
+		}
+		if (respuesta == null) return -1;;
+		if(respuesta.startsWith("tmin")) {
+			respuesta = respuesta.replace(";", "");
+			String[] sa = respuesta.split(" ");
+			Float f = Float.parseFloat(sa[1]);
+			return f;
+		}
+		return -1;
+	}
+	
+	public int setTempMin(final float tempMin){
+
+		respuesta = null;
+		
+		Thread t = new Thread( new Runnable() {
+			@Override
+			public void run() {
+				respuesta = cliente.enviarComando("set tmin " + tempMin +";");				
+			}
+		});
+		
+		t.start();
+		long start = Calendar.getInstance().getTimeInMillis();
+		while(t.isAlive() && Calendar.getInstance().getTimeInMillis() - start < 2000 ){}
+		
+		if(t.isAlive()){
+			t.stop();
+			return NO_RESPUESTA;
+		}
+		if (respuesta == null) return NO_CONEXION;
+		if(respuesta.startsWith("tmin")) {
+			return OK;
+		}
+		return ERROR_DESCONOCIDO;
+	}
+	
+	public float getTempMax(){
+		respuesta = null;
+		
+		Thread t = new Thread( new Runnable() {
+			@Override
+			public void run() {
+				respuesta = cliente.enviarComando("get tmax;");				
+			}
+		});
+		
+		t.start();
+		long start = Calendar.getInstance().getTimeInMillis();
+		while(t.isAlive() && Calendar.getInstance().getTimeInMillis() - start < 2000 ){}
+		
+		if(t.isAlive()){
+			t.stop();
+			return -1;
+		}
+		if (respuesta == null) return -1;;
+		if(respuesta.startsWith("tmax")) {
+			respuesta = respuesta.replace(";", "");
+			String[] sa = respuesta.split(" ");
+			Float f = Float.parseFloat(sa[1]);
+			return f;
+		}
+		return -1;
+	}
+	
+	public int setTempMax(final float tempMax){
+
+		respuesta = null;
+		
+		Thread t = new Thread( new Runnable() {
+			@Override
+			public void run() {
+				respuesta = cliente.enviarComando("set tmax " + tempMax +";");				
+			}
+		});
+		
+		t.start();
+		long start = Calendar.getInstance().getTimeInMillis();
+		while(t.isAlive() && Calendar.getInstance().getTimeInMillis() - start < 2000 ){}
+		
+		if(t.isAlive()){
+			t.stop();
+			return NO_RESPUESTA;
+		}
+		if (respuesta == null) return NO_CONEXION;
+		if(respuesta.startsWith("tmax")) {
+			return OK;
+		}
+		return ERROR_DESCONOCIDO;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+		
+	
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		Pecera p = new Pecera("192.168.1.15", "juan", 3);
+		InterfazMicro i = new InterfazMicro(p);
+		
+		while(true){
+			s.nextLine();
+			Calendar c = i.getDateTime();
+			System.out.println(c.getTimeInMillis());
+		}
+		
+	}
 }
+
