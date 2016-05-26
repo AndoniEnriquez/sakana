99d985a3cb26dc33c8e2a50e62347f0921869c85
HP <HP@Joseba>
2016-05-26 15:33:35 +0200

    tablas

--- src/tablas/ModeloTablaRegistroComida.java
+++ src/tablas/ModeloTablaRegistroComida.java
@@ -6,30 +6,30 @@ import javax.swing.table.AbstractTableModel;
 
 import ConexionDB.DAOComida;
 import ConexionDB.DAORegComida;
-import ConexionDB.DAOTipoComida;
 import VarTypes.Comida;
 import VarTypes.Pecera;
 import VarTypes.RegComida;
-import VarTypes.TipoComida;
 
+@SuppressWarnings("serial")
 public class ModeloTablaRegistroComida extends AbstractTableModel {
 
 	ModeloColumnasTablaRegistroComida columnas;
 	Pecera pecera;
 	ArrayList<RegComida> listaReservas;
 
-	public ModeloTablaRegistroComida (ModeloColumnasTablaRegistroComida columnas,Pecera pecera) throws Exception{
+	public ModeloTablaRegistroComida(ModeloColumnasTablaRegistroComida columnas, Pecera pecera) throws Exception {
 		super();
+		
 		this.columnas = columnas;
 		this.pecera = pecera;
-		listaReservas =  DAORegComida.getRegistrosPorPecera(pecera);
-
+		
+		listaReservas = DAORegComida.getRegistrosPorPecera(pecera);
 	}
 
-
-	public RegComida getReservaAt(int indice){
+	public RegComida getReservaAt(int indice) {
 		return listaReservas.get(indice);
 	}
+
 	@Override
 	public int getColumnCount() {
 
@@ -44,9 +44,10 @@ public class ModeloTablaRegistroComida extends AbstractTableModel {
 
 	@Override
 	public Object getValueAt(int fila, int columna) {
+		
 		RegComida a = listaReservas.get(fila);
-		return getFieldAt(a,columna);
-
+		
+		return getFieldAt(a, columna);
 	}
 
 	@Override
@@ -54,42 +55,44 @@ public class ModeloTablaRegistroComida extends AbstractTableModel {
 
 		return false;
 	}
+
 	@Override
 	public Class<?> getColumnClass(int columnIndex) {
 
-		return getValueAt(0,columnIndex).getClass();
+		return getValueAt(0, columnIndex).getClass();
 	}
 
-	public void actualizar() throws Exception {
-
-		/*DAORegComida.cleanReservas();
-		listaReservas = DAOReservas.getReservasRecurso(recurso);
-
-		this.fireTableDataChanged();*/
-	}
-
-	public Object getFieldAt(RegComida reserva,int columna) {
-		switch (columna){
-		case 0: return reserva.getId();
-		case 1: return reserva.getDatetime();
-		case 2: return this.getTipoComida(reserva);
-
-
+	public Object getFieldAt(RegComida reserva, int columna) {
+		
+		switch (columna) {
+		
+		case 0:
+			return reserva.getId();
+			
+		case 1:
+			return reserva.getDatetime();
+			
+		case 2:
+			return this.getTipoComida(reserva);
 		}
+		
 		return null;
 	}
 
-
 	private Object getTipoComida(RegComida reg) {
+		
 		String tipoComida = "";
+		
 		try {
+			
 			Comida c = DAOComida.buscarPorID(reg.getComida_id());
-			TipoComida tc = DAOTipoComida.buscarPorID(c.getTipocomida_id());
-			tipoComida = tc.getDescripcionTipoComida();
+			tipoComida = c.getNombreComida();
+			
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
+			
 			e.printStackTrace();
 		}
+		
 		return tipoComida;
 	}
 
