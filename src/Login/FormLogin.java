14dbedaa7b3eb4c66b9a128d0a7ff163c6cdd8bd
Anthonyenri <Anthonyenri@172.17.18.168>
2016-05-26 15:23:44 +0200

    sadf

--- src/Login/FormLogin.java
+++ src/Login/FormLogin.java
@@ -22,125 +22,124 @@ import Fabrica.FabricaAcciones;
 
 @SuppressWarnings("serial")
 public class FormLogin extends JFrame implements ActionListener {
-				
-  protected JButton btnOK,btnSalir;
-  protected JTextField txtUserName;
-  protected JPasswordField txtPassword;
-  protected JLabel lbUserName,lbPassword;
-  FabricaAcciones fabrica;
-  
 
-  
-  public FormLogin(FabricaAcciones fabrica) {
-  
-	  super("Login");
-    
-    this.setSize(320,210);
-    this.setLocation(550,300);
-    
-    this.fabrica = fabrica;
-    
-    this.setContentPane(crearPanelVentana());
-    this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-    this.setVisible(true);
-   }
-    
-    
-    private Container crearPanelVentana() {
-    	JPanel panel = new JPanel(new BorderLayout(0,10));
-    	panel.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
-    	
-    	panel.add(crearPanelCampos(), BorderLayout.CENTER);
-    	panel.add(crearPanelBotones(), BorderLayout.SOUTH);
-	 
-	   return panel;
-    }
-    
-	private Component crearPanelBotones() {
-		JPanel panel = new JPanel (new FlowLayout(FlowLayout.CENTER, 50,0));
+	protected JButton btnOK, btnSalir;
+	protected JTextField txtUserName;
+	protected JPasswordField txtPassword;
+	protected JLabel lbUserName, lbPassword;
+	FabricaAcciones fabrica;
+
+	public FormLogin(FabricaAcciones fabrica) {
+
+		super("Login");
+		this.setSize(320, 210);
+		this.setLocation(550, 300);
+		this.fabrica = fabrica;
+
+		this.setContentPane(crearPanelVentana());
+		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		this.setVisible(true);
+	}
+
+	private Container crearPanelVentana() {
 		
-		btnOK=new JButton(" OK ");
-	    btnOK.addActionListener(this);
-	    panel.add(btnOK);
-	    
-	    btnSalir=new JButton("Salir");
-	   
-	    btnSalir.addActionListener(this);
-	    panel.add(btnSalir);
-	    
+		JPanel panel = new JPanel(new BorderLayout(0, 10));
+		panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
+		panel.add(crearPanelCampos(), BorderLayout.CENTER);
+		panel.add(crearPanelBotones(), BorderLayout.SOUTH);
+
 		return panel;
 	}
-	
-	private Component crearPanelCampos() {
-		JPanel panel = new JPanel (new GridLayout(2,1));
 
-		panel.setBorder(BorderFactory.createCompoundBorder(
-				BorderFactory.createLineBorder(Color.PINK),BorderFactory.createEmptyBorder()));
+	private Component crearPanelBotones() {
+		
+		JPanel panel = new JPanel(new FlowLayout(FlowLayout.CENTER, 50, 0));
+
+		btnOK = new JButton(" OK ");
+		btnOK.addActionListener(this);
+		panel.add(btnOK);
+
+		btnSalir = new JButton("Salir");
+		btnSalir.addActionListener(this);
+		panel.add(btnSalir);
+
+		return panel;
+	}
+
+	private Component crearPanelCampos() {
+		
+		JPanel panel = new JPanel(new GridLayout(2, 1));
+		panel.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createLineBorder(Color.PINK),
+				BorderFactory.createEmptyBorder()));
 		panel.add(crearPanelUsername());
 		panel.add(crearPanelPassword());
-		return panel;
-	}
-	
-	private Component crearPanelPassword() {
-		JPanel panel = new JPanel (new GridLayout(1,2,30,0));
-		panel.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
-		lbPassword=new JLabel("Password :");
-	    lbPassword.setHorizontalAlignment(JLabel.RIGHT);
-	    panel.add(lbPassword);
-	    txtPassword=new JPasswordField(10);
-	    txtPassword.addActionListener(this);
-	    txtPassword.setBorder(BorderFactory.createLoweredBevelBorder());
-	    panel.add(txtPassword);
-		return panel;
-	}
-	
-	private Component crearPanelUsername() {
-		JPanel panel = new JPanel (new GridLayout(1,2,30,0));
-		panel.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
-	    lbUserName=new JLabel("Usuario :");
-	    lbUserName.setHorizontalAlignment(JLabel.RIGHT);
-	    panel.add(lbUserName);
-	    txtUserName=new JTextField(10);
-	    txtUserName.setBorder(BorderFactory.createLoweredBevelBorder());
-	    panel.add(txtUserName);
-		return panel;
-	}
-	
-    
-    
-  
-  @SuppressWarnings("static-access")
-public void actionPerformed(ActionEvent e){
-   
-	if((e.getSource() == btnOK)||(e.getSource() == this.txtPassword)) {
 		
-      LogCtrl logador=new LogCtrl();
-      
-      if(logador.validarUser(txtUserName.getText(),String.valueOf(txtPassword.getPassword()))) {
-    	  
-    	  //new FormRecursos(Sesion.getInstance().getUsuario());
-    	
-    	  fabrica.getPanelAjustes().labelUser.setText(txtUserName.getText());
-    	  fabrica.getPanelAjustes().bInicio.setEnabled(false);
-    	  fabrica.getPanelAjustes().bSalir.setEnabled(true);
-    	  fabrica.getPanelAjustes().bAdd.setEnabled(true);
-    	  fabrica.getPanelAjustes().bBloqueo.setEnabled(true);
-    	
-    	  this.setVisible(false);
-    	  this.dispose();
-      }
-      else {
-    	  JOptionPane.showMessageDialog(this,"Identificacion no valida", "Error de identificacion",
-    			    JOptionPane.ERROR_MESSAGE);
-      }
-    }
-    
-	else if(e.getSource()==btnSalir) {
-       this.dispose();
-    }
-  }
-  
-  
+		return panel;
+	}
 
-   
+	private Component crearPanelPassword() {
+		
+		JPanel panel = new JPanel(new GridLayout(1, 2, 30, 0));
+		panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		
+		lbPassword = new JLabel("Password :");
+		lbPassword.setHorizontalAlignment(JLabel.RIGHT);
+		panel.add(lbPassword);
+		
+		txtPassword = new JPasswordField(10);
+		txtPassword.addActionListener(this);
+		txtPassword.setBorder(BorderFactory.createLoweredBevelBorder());
+		panel.add(txtPassword);
+		
+		return panel;
+	}
+
+	private Component crearPanelUsername() {
+		
+		JPanel panel = new JPanel(new GridLayout(1, 2, 30, 0));
+		panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		
+		lbUserName = new JLabel("Usuario :");
+		lbUserName.setHorizontalAlignment(JLabel.RIGHT);
+		panel.add(lbUserName);
+		
+		txtUserName = new JTextField(10);
+		txtUserName.setBorder(BorderFactory.createLoweredBevelBorder());
+		panel.add(txtUserName);
+		
+		return panel;
+	}
+
+	@SuppressWarnings("static-access")
+	public void actionPerformed(ActionEvent e) {
+
+		if ((e.getSource() == btnOK) || (e.getSource() == this.txtPassword)) {
+
+			LogCtrl logador = new LogCtrl();
+
+			if (logador.validarUser(txtUserName.getText(), String.valueOf(txtPassword.getPassword()))) {
+
+				fabrica.getPanelAjustes().labelUser.setText(txtUserName.getText());
+				fabrica.getPanelAjustes().bInicio.setEnabled(false);
+				fabrica.getPanelAjustes().bSalir.setEnabled(true);
+				fabrica.getPanelAjustes().bAdd.setEnabled(true);
+				fabrica.getPanelAjustes().bBloqueo.setEnabled(true);
+
+				this.setVisible(false);
+				this.dispose();
+				
+			} else {
+				
+				JOptionPane.showMessageDialog(this, "Identificacion no valida", "Error de identificacion",
+						JOptionPane.ERROR_MESSAGE);
+			}
+		}
+
+		else if (e.getSource() == btnSalir) {
+			
+			this.dispose();
+		}
+	}
+
 }
--- src/Login/Sesion.java
+++ src/Login/Sesion.java
@@ -4,21 +4,28 @@ import VarTypes.Dueno;
 
 public class Sesion {
 
-		
-		  
-	protected Dueno p;	
-	private Sesion() {}
-	private static Sesion instance;
-	public static Sesion getInstance() {
-		
-	if(instance==null)
-		
-			instance=new Sesion();
-		    return instance;
-		 
+	protected Dueno p;
+
+	private Sesion() {
 	}
-	
-	public void setUsuario(Dueno p){this.p=p;}
-	public Dueno getUsuario() {return p;}
-		
+
+	private static Sesion instance;
+
+	public static Sesion getInstance() {
+
+		if (instance == null)
+
+			instance = new Sesion();
+		return instance;
+
+	}
+
+	public void setUsuario(Dueno p) {
+		this.p = p;
+	}
+
+	public Dueno getUsuario() {
+		return p;
+	}
+
 }
